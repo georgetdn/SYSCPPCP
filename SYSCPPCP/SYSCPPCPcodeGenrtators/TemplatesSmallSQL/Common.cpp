a95fc99b311cee9467f78d9f1b8d9d54b2b4f955
georgetdn <gdobrescu@tdnsys.com>
2024-11-04 18:51:05 -0500

    Updated SQL comment

--- SYSCPPCP/SYSCPPCPcodeGenrtators/TemplatesSmallSQL/Common.cpp
+++ SYSCPPCP/SYSCPPCPcodeGenrtators/TemplatesSmallSQL/Common.cpp
@@ -206,6 +206,7 @@ std::string LoadHelp(void )
 {
     return
         "Only one class(table) can be used in a query\n"
+        "For empty string or character variables (columns) use \" \""
         "Maximum of five where clauses.\n"
         "Except for arrays of char, no arrays can be used in a query\n"
         "For enumerations, you have to use the item name, not the associated integer value\n"
